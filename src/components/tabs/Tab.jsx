51f68163beada1d1e3adc597cf51d400b8a2118a
Lyndsey Browning <lbrowning86@gmail.com>
2021-03-17 17:14:25 +0000

    Initial commit

---
+++ src/components/tabs/Tab.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Tab = (props) => {
+    return <div>Hey</div>;
+};
+
+Tab.propTypes = {};
+
+export default Tab;
---
+++ src/components/tabs/Tabs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Tabs = (props) => {
+    return <div>Hey</div>;
+};
+
+Tabs.propTypes = {};
+
+export default Tabs;
